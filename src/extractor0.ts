fc50b36e889d3e7ab4582caa276ea47d2cb389a3
Leonardo Muñoz <contacto@pctester.cl>
2024-05-02 00:59:06 -0400

    add text encoding and pdf-parse to text

--- src/extractor0.ts
+++ src/extractor0.ts
@@ -3,12 +3,13 @@ import dotenv from "dotenv"
 import axios from "axios"
 import { HfInference } from "@huggingface/inference";
 import { readMyFile } from "./utils/readFiles"
-import { pipeline } from "@xenova/transformers";
+import pdfParse, { type Result } from "pdf-parse"
+// import { pipeline } from "@xenova/transformers";
 
 dotenv.config()
 
 async function main() {
-    
+
     const token = process.env.API
     const workerID = process.env.WORKER
     const url = "https://worker.formextractorai.com/v2/extract"
@@ -17,50 +18,64 @@ async function main() {
     console.log(pdfFile)
     const pdfBuffer = await readMyFile(pdfFile)
     const pdfBase64 = Buffer.from(pdfBuffer).toString('base64')
-    const pdfText:string = '';
-       
-           async function ResultApi(pdfText:string) {
-            axios.post(url, pdfBase64, {
-                headers: {
-                    "X-WORKER-TOKEN": token,
-                    "X-WORKER-EXTRACTOR-ID": workerID,
-                    "Content-Type": "image/png",
-                    "X-WORKER-ENCODING": "base64"
-                }
-            
-            }).then((response) => {
-                console.log(response.data)
 
-                const pdfText = JSON.stringify(response.data)
-                console.log(pdfText)
+    async function getTextFromPdf(file: Buffer): Promise<string> {
+        const result: Result = await pdfParse(file, {
 
-                return Promise.resolve(pdfText);
+        })
+        // console.log(result.text)
+        return tokenizar(result.text)
+    }
+    function tokenizar(texto: string): string {
+        const encoder = new TextEncoder();
+        const utf8Bytes = encoder.encode(texto);
+        const decoder = new TextDecoder('iso-8859-1');
+        const textoISO = decoder.decode(utf8Bytes);
+        return textoISO.split(/\W+/).toString().replace(/,/g, ' ').replace(/\n/g, ' ')
+    }
 
-            }).catch((error) => {
-                console.log("error de response "+error)
-            })
-           }
-            
-     
+    async function getStringJsonDataFromFormExtractor(pdfText: string) {
+        const { data, status } = await axios.post(url, pdfBase64, {
+            headers: {
+                "X-WORKER-TOKEN": token,
+                "X-WORKER-EXTRACTOR-ID": workerID,
+                "Content-Type": "image/png",
+                "X-WORKER-ENCODING": "base64"
+            }
 
+        })
+        if (data == 'failed') {
+            console.log("no data")
+            return ""
+        }
+        console.log(data.documents[0])
+        return JSON.stringify(data.documents)
+        // const pdfString = JSON.stringify()
+    }
+
+
+    console.log("###################################")
     const hfEnv = process.env.HF
     const hf: HfInference = new HfInference(hfEnv);
-    const model = "bert-base-uncased";
-    const context:string = await ResultApi(pdfText)
-      
+    // const model = "bert-base-uncased";
+    const model = "meta-llama/Meta-Llama-3-8B";
+    const pdfTexto = await getTextFromPdf(pdfBuffer)
+    const context: string = tokenizar(pdfTexto)
+    console.log("contexto ", context)
+
     try {
-        const result  = await hf.questionAnswering({
-            model:model,
-            inputs:{
+        const result = await hf.questionAnswering({
+            model: model,
+            inputs: {
                 context: context,
-               question:'Cual es el Total Neto',
-              
+                question: 'Cual es el monto Neto',
             }
         });
-         console.log(result)
-     } catch (error) {
-        console.log('Error a la peticion de IA ')
-     }
+        console.log(result)
+    } catch (error) {
+        console.log("Error en peticion")
+        console.log('detalle error ', error)
+    }
 
 
 
